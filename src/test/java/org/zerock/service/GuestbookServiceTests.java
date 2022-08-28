e712777a30232583318d22359a136e3328464d98
WS-SHIM <110947438+WS-SHIM@users.noreply.github.com>
2022-08-28 14:39:58 +0900

    register 테스트

--- src/test/java/org/zerock/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/service/GuestbookServiceTests.java
@@ -1,16 +1,24 @@
 package org.zerock.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.dto.GuestbookDTO;
 import org.zerock.entity.Guestbook;
+import org.zerock.repository.GuestbookRepository;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class GuestbookServiceTests {
     @Autowired
     private GuestbookService service;
+    @Autowired
+    private GuestbookRepository guestbookRepository;
 
+    @DisplayName("신규 게시물 등록 테스트")
     @Test
     public void testRegister() {
         GuestbookDTO guestbookDTO = GuestbookDTO.builder()
@@ -18,7 +26,16 @@ public class GuestbookServiceTests {
                 .content("Sample content...")
                 .writer("user0")
                 .build();
-        System.out.println(service.register(guestbookDTO));
+
+        Long gno = service.register(guestbookDTO);
+
+        Optional<Guestbook> byId = guestbookRepository.findById(gno);
+        Guestbook guestbook = byId.get();
+
+        Assertions.assertEquals(guestbookDTO.getContent(), guestbook.getContent());
+        Assertions.assertEquals(guestbookDTO.getTitle(), guestbook.getTitle());
+        Assertions.assertEquals(guestbookDTO.getWriter(), guestbook.getWriter());
+
     }
 
 }
