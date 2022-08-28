26c942e77b084b901f715df75ffbad97b8980aa1
WS-SHIM <110947438+WS-SHIM@users.noreply.github.com>
2022-08-28 14:22:36 +0900

    save로직 추가

--- src/main/java/org/zerock/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/service/GuestbookServiceImpl.java
@@ -1,15 +1,20 @@
 package org.zerock.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.zerock.dto.GuestbookDTO;
 import org.zerock.entity.Guestbook;
+import org.zerock.repository.GuestbookRepository;
 
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl  implements GuestbookService {
+
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -17,6 +22,8 @@ public class GuestbookServiceImpl  implements GuestbookService {
         log.info(dto);
         Guestbook entity = dtoToEntity(dto);
         log.info(entity);
-        return null;
+
+        repository.save(entity);
+        return entity.getGno();
     }
 }
