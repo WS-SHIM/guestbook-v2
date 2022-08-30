8c09c6d8d4a63e01bc156a854471a4448b8fd382
WS-SHIM <110947438+WS-SHIM@users.noreply.github.com>
2022-08-30 21:56:15 +0900

    PageResultDTO 완성

--- src/test/java/org/zerock/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/service/GuestbookServiceTests.java
@@ -45,9 +45,16 @@ public class GuestbookServiceTests {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
 
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
+
+        resultDTO.getPageList().forEach(i -> System.out.println("i = " + i));
     }
 
 
